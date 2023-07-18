aa71352437457caf7b1d55ee9e22066b92938cb2
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-07-18 17:36:51 -0500

    cleanup find_manifests
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- scripts/find_manifests.py
+++ scripts/find_manifests.py
@@ -1,16 +1,19 @@
-import os
+"""Search for Kubneretes manifests in the kubernetes/manifests/ directory."""
+
 from pathlib import Path
 
-potential_manifests = Path("./kubernetes/manifests/").glob("**/*.yaml")
 
-likely_manifests = []
+def run() -> None:
+    """Search for Kubneretes manifests in the kubernetes/manifests/ directory."""
+    likely_manifests = [
+        str(file)
+        for file in Path("./kubernetes/manifests/").glob("**/*.yaml")
+        if "apiVersion:" in file.read_text()  # File is likely a k8s manifests
+        and not file.stem.startswith("_")  # Ignore manifests that start with _
+    ]
 
-for file in potential_manifests:
-    if "apiVersion:" in file.read_text():
-    # File is likely a k8s manifests
+    print("\n".join(likely_manifests))
 
-        # Ignore manifests that start with _
-        if not file.stem.startswith("_"):
-            likely_manifests.append(str(file))
 
-print("\n".join(likely_manifests))
+if __name__ == "__main__":
+    run()
