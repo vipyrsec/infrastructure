9349a4f172377e91459242416fd7594484d35d93
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-07-18 17:24:13 -0500

    move db setup script back to scripts
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

---
+++ scripts/bootstrap-db.sql
@@ -0,0 +1,12 @@
+-- Dragonfly
+-- Create the database
+CREATE DATABASE dragonfly OWNER dragonfly;
+-- Create an admin role
+CREATE ROLE dragonfly_admin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO dragonfly_admin;
+-- Create a read-only role
+CREATE ROLE dragonfly_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO dragonfly_read;
+-- Create initial user roles
+CREATE ROLE bradley WITH PASSWORD 'shadow' IN ROLE dragonfly_admin LOGIN;
+CREATE ROLE robin WITH PASSWORD 'shadow' IN ROLE dragonfly_read LOGIN;
